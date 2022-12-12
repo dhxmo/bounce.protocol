9be2711fc4a1905c15e6bb6aacc0a91ee19d3484
zdhxmo <dhrvmohapatra@gmail.com>
2022-12-12 14:08:11 +0530

    contracts and frontend boilerplate

---
+++ contracts/src/interfaces/IBounce.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+interface IBounce {
+
+    struct Order {
+        address fromToken;
+        address toToken;
+        address toAddress;
+        address BounceReceiver;
+        uint256 fromTokenAmount;
+        uint256 minAmtToToken;
+        uint256 relayerFee;
+        uint256 slippage;
+        uint32 toDomainID_Connext;
+    }
+
+    function BounceFrom(Order memory _order) external payable returns(bytes32);
+
+    function BounceTo(
+        address toAddress,
+        address toToken,
+        uint256 minToTokenAmount,
+        bytes calldata payload    
+    ) external returns (uint256);
+}
