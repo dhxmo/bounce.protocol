8c08f52e623aeb005b24c5de18a12609e62b8aef
zdhxmo <dhrvmohapatra@gmail.com>
2022-12-12 14:53:10 +0530

    feat: basic folder structure setup

---
+++ client/components/index.js
@@ -0,0 +1,8 @@
+import Swap from "./Swap";
+import Model from "./Model";
+import Navbar from "./Navbar";
+import Search from "./Search";
+import Toggle from "./Toggle";
+import List from "./List";
+
+export { Swap, Model, Navbar, Search, Toggle, List };
